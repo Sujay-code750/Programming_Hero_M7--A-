8c5ec70677eaa8559bb06bca9b0eca794b2e2e69
Sujoy <sujaykarmokar86@gmail.com>
2026-05-25 12:58:29 +0600

    create Satus section

--- src/pages/StatusPage.jsx
+++ src/pages/StatusPage.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const StatusPage = () => {
     return (
-        <div>
-            StatusPage
+        <div className='py-20 bg-[#f8fafc]'>
+           <div className='container mx-auto'>
+                <h1 className='font-bold text-5xl mb-10'>Friendship Analytics</h1>
+           </div>
         </div>
     );
 };
